fa5c3adc16e4060b0c438126e1fcb1b2c9627bbe
Mihai <mihai.banulescu@gmail.com>
2016-08-29 18:22:00 -0700

    ...

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,28 +1,20 @@
 class GamesController < ApplicationController
   before_action :set_game, only: [:show, :edit, :update, :destroy]
 
-  # GET /games
-  # GET /games.json
   def index
     @games = Game.all
   end
 
-  # GET /games/1
-  # GET /games/1.json
   def show
   end
 
-  # GET /games/new
   def new
     @game = Game.new
   end
 
-  # GET /games/1/edit
   def edit
   end
 
-  # POST /games
-  # POST /games.json
   def create
     @game = Game.new(game_params)
 
@@ -37,8 +29,6 @@ class GamesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /games/1
-  # PATCH/PUT /games/1.json
   def update
     respond_to do |format|
       if @game.update(game_params)
@@ -51,8 +41,6 @@ class GamesController < ApplicationController
     end
   end
 
-  # DELETE /games/1
-  # DELETE /games/1.json
   def destroy
     @game.destroy
     respond_to do |format|
@@ -62,13 +50,12 @@ class GamesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_game
       @game = Game.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def game_params
-      params.require(:game).permit(:starting, :ending, :location_id, :host_id, :facebook_event_url, :review)
+      params.require(:game).permit(:starting, :ending, :location_id, :host_id,
+        :facebook_event_url, :review, activity_ids: [])
     end
 end
