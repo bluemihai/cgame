c0e430b5d28e2bdd95aeeba14d171221c4d834ef
Mihai <mihai.banulescu@gmail.com>
2016-09-01 23:53:45 -0700

    random wild card implemented

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,6 @@
 class VisitorsController < ApplicationController
+  def wildcard
+    rand_wc = Activity.wild_card.sample
+    redirect_to activity_path(rand_wc)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :activities
   resources :users
   root to: 'activities#index'
+  get '/w_c' => 'visitors#wildcard', as: :wildcard
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
