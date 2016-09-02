48d033c0e462e552e394de19c7150792477d4db0
Mihai <mihai.banulescu@gmail.com>
2016-09-01 22:58:42 -0700

    randomize ids

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,10 +1,22 @@
 class Activity < ApplicationRecord
+  before_create :randomize_id
 
   scope :wild_card, -> { where('wild_card_weight > 0').where(active: true) }
   scope :non_wild_card, -> { where(wild_card_weight: 0).where(active: true) }
   scope :main, -> { where(main: true) }
 
-  def key
-    
+  def update_id_as_random
+    begin
+      self.id = SecureRandom.random_number(1_000_000)
+    end while Activity.where(id: self.id).exists?
+    self.save!
   end
+
+  private
+    def randomize_id
+      begin
+        self.id = SecureRandom.random_number(1_000_000)
+      end while Activity.where(id: self.id).exists?
+    end
+
 end
