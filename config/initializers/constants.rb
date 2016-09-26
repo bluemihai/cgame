faaf6adbb83a81072890f60b9666f299d3348939
Mihai <mihai.banulescu@gmail.com>
2016-09-26 14:42:23 -0600

    update values

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,6 @@
 VALUES = {
   'C for Connection' => 'The desire to connect is fundamental to C-Game. We prefer activities that connect us, rather than inform, heal or stimulate us. We co-host and co-create, because that too leads to more connection.',
-  'Honor Self' => 'C-Game is a high-level game for skilled and willing players. There will be a minimum container.  Beyond that, we expect everyone to honor their boundaries and request whatever they need.',
+  'Honor Thy Self' => 'C-Game is a high-level game for skilled and willing players. There will be a minimum container.  Beyond that, we expect everyone to honor their boundaries and request whatever they need.',
   'Group Wisdom' => 'Groups can curate the Wild Card deck and co-hosts can add their own Main Courses because collective intelligence trumps what any one person can come up with. Besides, people tend to have more fun when involved!',
   'Gifting Energy' => 'C-Game started as a gift shared among friends, and we strive to maintain that energy as it grows. Gifting is a sacred exchange performed in conscious relationship of equals.  The gifter honors herself and offers freely, with no strings attached.  The receiver appreciates and accepts the gift.',
   'Join Us' => 'C-Game is self-organized and energized by a handful of people. Please ask a host how you can join us as a regular attendee, host or partner.'
